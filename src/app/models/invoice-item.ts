86670118697cd2813ec60f1a0d9e7dda5843c453
Richard <Richard.jc20@hotmail.com>
2025-03-26 10:25:47 -0500

    eliminacion de invoices

--- src/app/models/invoice-item.ts
+++
@@ -1,6 +0,0 @@
-export interface InvoiceItem {
-    productName: string; // Nombre del producto
-    quantity: number;     // Cantidad comprada
-    total: number;        // Precio total (incluyendo impuestos)
-    id_products: number;
-  }
