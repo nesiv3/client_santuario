a94615ac434a91ce6697de75bbca9fc6e6296592
David Sanchez <dasava11@outlook.com>
2025-04-08 07:12:14 -0500

    login ajustado con mensajes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
+    standalone: true, // si este componente es standalone
+    imports: [CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+welcomeMessage: string = '';
+
+    ngOnInit(): void {
+        const message = sessionStorage.getItem('welcome');
+        if (message) {
+          this.welcomeMessage = message;
+          sessionStorage.removeItem('welcome'); // lo elimina para que no se repita
+        }
+      }
 }
